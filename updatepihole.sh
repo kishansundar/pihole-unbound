6d29ce5a69daaec3f8cf40fb054a5fa4676f4526
Kishan <2255726+kishansundar@users.noreply.github.com>
2020-05-30 02:20:43 +0530

    Updated sources: 05/30/20 02:20:43

--- updatepihole.sh
+++ updatepihole.sh
@@ -1,25 +1,2 @@
 
-#curl https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -o /root/scripts/hosts
-bash /root/scripts/hblock -O  /root/scripts/hosts
-set -e
-
-SRC="/root/scripts/hosts"
-OUTPUT="/etc/unbound/ads/ads.conf"
-
-rm -Rfr "$OUTPUT"
-
-if [ ! -f "$SRC" ]; then
-    echo "Could not open $SRC"
-    exit 1
-fi
-
-awk '/^0\.0\.0\.0/ {
-    print "local-zone: \""$2"\" redirect"
-    print "local-data: \""$2" A 0.0.0.0\""
-}' "$SRC" > "$OUTPUT"
-
-echo "done"
-
-rm -Rfr "$SRC"
-
-#/usr/local/bin/pihole -g
+/usr/local/bin/pihole -g
