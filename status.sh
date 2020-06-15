0d2a7cd54e5adbe45934b06f620c4f8ea955f82e
Kishan <2255726+kishansundar@users.noreply.github.com>
2020-06-15 10:34:23 +0530

    Updated sources: 06/15/20 10:34:23

--- status.sh
+++ status.sh
@@ -1,8 +1,8 @@
 echo "Pihole... Status"
 sudo systemctl status pihole-FTL
-echo "NSD... Status"
-sudo systemctl status nsd
+#echo "NSD... Status"
+#sudo systemctl status nsd
 echo "Unbound... Status"
 sudo systemctl status unbound
-echo "Stubby... Status"
-sudo systemctl status stubby
+#echo "Stubby... Status"
+#sudo systemctl status stubby
