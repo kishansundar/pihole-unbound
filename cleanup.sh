f430a7a164598e91bc5306d81e293faf5ddcc363
Kishan <2255726+kishansundar@users.noreply.github.com>
2020-10-03 16:56:34 +0530

    Updated sources: 10/03/20 16:56:34

--- cleanup.sh
+++ cleanup.sh
@@ -6,14 +6,13 @@ echo "Deleting Pihole DB..."
 rm -Rfr /etc/pihole/pihole-FTL.db
 echo "Removing Logs..."
 rm -Rfr /var/log/*
-echo "Restarting Pihole..."
-sudo systemctl restart pihole-FTL
-#echo "Restarting NSD..."
-#sudo systemctl restart nsd
 echo "Restarting Unbound..."
 sudo systemctl restart unbound
 #echo "Restarting Stubby..."
 #sudo systemctl restart stubby
+
+echo "Restarting Pihole..."
+sudo systemctl restart pihole-FTL
 
 echo "Done."
 
