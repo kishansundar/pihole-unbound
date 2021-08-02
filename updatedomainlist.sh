431a9808ecf17ae6efc242418dc08c0a7f2bc231
Kishan <2255726+kishansundar@users.noreply.github.com>
2021-08-02 11:45:46 +0530

    update list

--- updatedomainlist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sqlite3 /etc/pihole/gravity.db "delete from domainlist;" \
-&& sqlite3 /etc/pihole/gravity.db "delete from domainlist_by_group" \
-&& sqlite3 /etc/pihole/gravity.db "DELETE FROM 'sqlite_sequence' WHERE name='domainlist'" \
-&& pihole -w $(cat /root/block/whitelist/white.list) \
-&& pihole --regex $(cat /root/block/regex/regex.list)
