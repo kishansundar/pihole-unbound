a66bca493b29d8390f64850fb11dee11f7a753e2
Kishan <2255726+kishansundar@users.noreply.github.com>
2021-08-06 03:49:03 +0530

    Updated sources: 08/06/21 03:49:03

--- unbound-stable.sh
+++ unbound-stable.sh
@@ -4,10 +4,12 @@ version=1.13.1
 
 wget https://nlnetlabs.nl/downloads/unbound/unbound-${version}.tar.gz
 
+#https://nlnetlabs.nl/downloads/unbound/unbound-latest.tar.gz
+
 tar -xzf unbound-${version}.tar.gz && rm -Rfr unbound-${version}.tar.gz
-rm -Rfr unbound-stable
-mv unbound-${version} unbound-stable
-cd ~/unbound-stable
+rm -Rfr unbound-latest
+mv unbound-${version} unbound-latest
+cd ~/unbound-latest
 
 ./configure --prefix=/usr --sysconfdir=/etc --disable-static --with-pidfile=/etc/unbound/unbound.pid --with-libevent --enable-dnscrypt --with-pthreads --disable-systemd --enable-pie --with-libnghttp2
  make && sudo make install
@@ -20,4 +22,4 @@ cd ~/unbound-stable
 
  unbound-control-setup
 
- rm -Rfr ~/unbound-stable
+ rm -Rfr ~/unbound-latest
