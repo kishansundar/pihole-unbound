5acc05f2f6295ab1510bc64e3dd7b936f44a25a3
Kishan <2255726+kishansundar@users.noreply.github.com>
2021-08-05 09:44:24 +0530

    Updated sources: 08/05/21 09:44:24

--- cleanup.sh
+++ cleanup.sh
@@ -5,11 +5,14 @@ sudo systemctl stop pihole-FTL
 echo "Deleting Pihole DB..."
 rm -Rfr /etc/pihole/pihole-FTL.db
 echo "Removing Logs..."
+/usr/local/bin/pihole -f
 rm -Rfr /var/log/*
 echo "Restarting Unbound..."
 sudo systemctl restart unbound
 #echo "Restarting Stubby..."
 #sudo systemctl restart stubby
+echo "ARP FLUSH...."
+/usr/local/bin/pihole arpflush
 
 echo "Restarting Pihole..."
 sudo systemctl restart pihole-FTL
