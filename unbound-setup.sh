799a78a082bf3e9e7931a637ff9cbc55187120e5
Kishan <2255726+kishansundar@users.noreply.github.com>
2021-10-13 17:34:58 +0530

    Updated sources: 10/13/21 17:34:58

--- unbound-setup.sh
+++ unbound-setup.sh
@@ -3,4 +3,4 @@
 sudo groupadd -g 88 unbound
 sudo useradd -c "Unbound DNS resolver" -d /var/lib/unbound -u 88 -g unbound -s /bin/false unbound
 
-sudo apt install libssl-dev libevent-dev libexpat-dev libnghttp2-dev libsodium-dev
+sudo apt install libssl-dev libevent-dev libexpat-dev libnghttp2-dev libsodium-dev -y
