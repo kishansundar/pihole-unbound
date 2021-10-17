5db2f12f4929f8226869d0a129c9433437975517
Kishan <2255726+kishansundar@users.noreply.github.com>
2021-10-17 17:17:47 +0530

    Updated sources: 10/17/21 17:17:47

--- post-install.sh
+++ post-install.sh
@@ -18,8 +18,11 @@ echo "read-ethers" >> /etc/dnsmasq.d/03-pihole-extra.conf
 echo "------------------------------------------"
 echo "Updating... Hosts"
 echo "127.0.1.2 unbound-ipv4" >> /etc/hosts
-echo "::1 unbound-ipv6" >> /etc/hosts
-echo "192.168.1.1 Router.home" >> /etc/hosts
+# ::1 localhost ip6-localhost ip6-loopback
+sed -i -e 's/::1 localhost ip6-localhost ip6-loopback/::1 unbound-ipv6 localhost ip6-localhost ip6-loopback/g'
+sed -i -e 's/127.0.0.1 localhost/127.0.0.1 Dietpi localhost/g'
+echo "192.168.1.1 Router" >> /etc/hosts
+echo "192.168.1.2 Orbi" >> /etc/hosts
 
 echo "------------------------------------------"
 echo "Enabling... Services"
